0400c309fac42cab8f7fd87c48fd3704eea9a39d
Josh Suson <josh.suson@gmail.com>
2019-11-07 11:07:11 -0500

    serch function added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
+
 //Lightgallery Plugin
 $(document).ready(function() {
         $("#lightgallery").lightGallery();
     });
+
+//Search function
+
+$("#search-bar").keypress(function() {
+  let currentQuery = $("#search-bar").val().toLowerCase();
+  $(".gallery a").each(function() {
+    let currentKeyword = $(this).attr("data-alt").toLowerCase();
+    if (currentKeyword.indexOf(currentQuery) >= 0) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
